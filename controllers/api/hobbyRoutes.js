6184568363e7c4463ba2a63563545ffe92d316b3
Billy Mott <92488759+Billygm@users.noreply.github.com>
2022-02-19 13:28:18 -0800

    adding profile routes

--- controllers/api/hobbyRoutes.js
+++ controllers/api/hobbyRoutes.js
@@ -18,4 +18,25 @@ router.post("/", withApiAuth, async (req, res) => {
   });
   res.json(hobbies);
 });
+
+router.delete("/:id", withApiAuth, async (req, res) => {
+  try {
+    const hobbyData = await Hobby.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!hobbyData) {
+      res.status(404).json({ message: "No hobby found with this id!" });
+      return;
+    }
+
+    res.status(200).json(hobbyData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
