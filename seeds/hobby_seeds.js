6e31972d82bde52ffd58b194ca47e57924d9534e
Avalynn Judge <avalynnjudge@gmail.com>
2022-02-19 13:14:39 -0800

    bug fixes

--- seeds/hobby_seeds.js
+++ seeds/hobby_seeds.js
@@ -1,14 +1,14 @@
-const { Category } = require('../models');
+const { Category } = require("../models");
 
 const categoryData = [
   {
-    category_name: 'books',
+    category_name: "books",
   },
   {
-    category_name: 'music',
+    category_name: "music",
   },
   {
-    category_name: 'movies',
+    category_name: "movies",
   },
 ];
 
